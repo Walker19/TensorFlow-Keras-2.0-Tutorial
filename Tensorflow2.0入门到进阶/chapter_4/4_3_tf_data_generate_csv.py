2d9320b91e52ea77056f3db8b7eebd6b34bf8ab5
yangwei <yangwei02@purvar.com>
2020-02-21 09:19:08 +0800

    modify 4_3

--- Tensorflow2.0入门到进阶/chapter_4/4_3_tf_data_generate_csv.py
+++ Tensorflow2.0入门到进阶/chapter_4/4_3_tf_data_generate_csv.py
@@ -74,5 +74,74 @@ valid_filenames = save_to_csv(output_dir, valid_data, "valid",
 
 test_filenames = save_to_csv(output_dir, test_data, "test",
                              header_str, n_parts=20)
+
+import pprint
+
+print("train filenames:")
+pprint.pprint(train_filenames)
+print("valid filenames:")
+pprint.pprint(valid_filenames)
+print("test filenames:")
+pprint.pprint(test_filenames)
+
+# 如何将上述文件路径名列表读取为关于内容的 dataset，两个步骤：
+# 1.filename -> dataset
+# 2.read file ->(interl) dataset -> datasets -> merge
+# 3.parse 解析csv文件
+
+# 第一步：转化为文件名 dataset
+filename_dataset = tf.data.Dataset.list_files(train_filenames)
+# list_files 专门将文件名转化为 dataset
+for filename in filename_dataset:
+    print(filename)
+
+
+# 第二步，将文件名dataset读取为真实dataset
+n_readers = 5
+dataset = filename_dataset.interleave(
+    lambda filename: tf.data.TextLineDataset(filename).skip(1),
+    # TextLineDataset,按行读取文件内容
+    # skip(1),跳过数据的第一行(标题栏)
+    cycle_length=n_readers
+)
+
+for line in dataset.take(15):
+    print(line.numpy())
+    # '0.04049225382803661,-0.6890414153725881,-0.44379851741607473,
+    # 0.022374585146687852,-0.22187226486997497,-0.1482850314959248,-0.8883662012710817,
+    # 0.6366409215825501,2.852'
+    # 这是csv的一行，分别展开的8个字段，最后个是label
+
+
+# 第三步：解析csv
+# tf.io.decode_csv(str, record_defaults),str是需要转换的字符串，
+# record_defaults是将该字段转换为特定数据类型
+# 注意，我们这里是对csv每行(str)处理
+
+# 示例
+sample_str = '1,2,3,4,5'
+# record_defaults 1:
+# record_defaults = [tf.constant(0, dtype=tf.int32)] * 5
+
+# record_defaults 2:
+record_defaults = [
+    tf.constant(0, dtype=tf.int32),
+    0,
+    np.nan,
+    'hello',
+    tf.constant([])
+]
+parsed_fields = tf.io.decode_csv(sample_str, record_defaults)
+# record_defaults中的变量只是作为参照，来将sample_str中的变量转为同样数据类型
+print(parsed_fields)
+
+
+# 如果传入错误字符串怎么办？
+try:
+    parsed_fields = tf.io.decode_csv(',,,,',  record_defaults)
+    # parsed_fields2 = tf.io.decode_csv('1,2,3,4,5,6,7',  record_defaults)
+except tf.errors.InvalidArgumentError as ex:
+    print(ex)
+
 # continue
-# https://www.bilibili.com/video/av79196096?p=46
+# https://www.bilibili.com/video/av79196096?p=47
